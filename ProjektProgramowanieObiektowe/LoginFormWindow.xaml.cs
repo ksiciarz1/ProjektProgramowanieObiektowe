5d0933afcc8dc64801cf4e5a2a5875486589b25b
ksiciarz <kacpersiciarz2001@gmail.com>
2022-04-11 14:21:04 +0200

    Eksperymenty z WPF

---
+++ ProjektProgramowanieObiektowe/LoginFormWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektProgramowanieObiektowe
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginFormWindow.xaml
+    /// </summary>
+    public partial class LoginFormWindow : Window
+    {
+        public int i = 0;
+        public LoginFormWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Used for loging in to access MainWindow
+        /// </summary>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CoolButton.Content = i;
+            i++;
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
