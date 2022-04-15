13f292046625f6cbe3f0623481eacba0fe33118c
ksiciarz <kacpersiciarz2001@gmail.com>
2022-04-15 23:15:40 +0200

    Changed ListBox to DataGrid
    Used DataGrid with automatic Column Headers.

---
+++ ProjektProgramowanieObiektowe/BooksForm.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektProgramowanieObiektowe
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class BookForm : Window
+    {
+        public BookForm()
+        {
+            InitializeComponent();
+
+            ObservableCollection<Book> custData = new ObservableCollection<Book>();
+
+            Book book = new Book();
+            book.Name = "Dramat";
+            book.Author = "Adam Mickiewicz";
+            book.Category = "Dramat";
+            book.PublishingHouse = "Operon";
+
+            custData.Add(book);
+
+            mainDataGrid.DataContext = custData;
+        }
+    }
+}
--- ProjektProgramowanieObiektowe/MainWindow.xaml.cs
+++ ProjektProgramowanieObiektowe/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,15 +12,17 @@ namespace ProjektProgramowanieObiektowe
     public partial class MainWindow : Window
     {
         LibraryContext database;
-        ListBox[] listBoxes;
 
         public MainWindow()
         {
             InitializeComponent();
-            listBoxes = new ListBox[] { Lisbox1, Listbox2, Listbox3 };
 
             // Getting data from server and displaying in ListBox1
             RefreshButton_Click(null, null);
+
+            // HACK: To Delete
+            BookForm bookForm = new BookForm();
+            bookForm.Show();
 
         }
 
@@ -30,10 +33,6 @@ namespace ProjektProgramowanieObiektowe
         private void RefreshButton_Click(object? sender, RoutedEventArgs? e)
         {
             database = new LibraryContext();
-
-            // Clears Listboxes
-            foreach (ListBox listbox in listBoxes)
-                listbox.Items.Clear();
 
             #region Filters
 
@@ -50,47 +49,14 @@ namespace ProjektProgramowanieObiektowe
                  book.Author.Contains(authorFilter)
                 ).ToArray();
 
+            ObservableCollection<Book> bookColection = new();
+
             // Displaying data in ListBoxes
             foreach (Book book in books)
             {
-                ListBoxItem listBoxId = new ListBoxItem();
-                ListBoxItem listBoxName = new ListBoxItem();
-                ListBoxItem listBoxAuthor = new ListBoxItem();
-
-                // TODO: Formatting the ListBox1 strings nicely
-                // String format to display in ListBox1
-                listBoxId.Content = $"{book.Id}";
-                listBoxName.Content = $"{book.Name}";
-                listBoxAuthor.Content = $"{book.Author}";
-
-                listBoxes[0].Items.Add(listBoxId);
-                listBoxes[1].Items.Add(listBoxName);
-                listBoxes[2].Items.Add(listBoxAuthor);
+                bookColection.Add(book);
             }
-        }
-
-        /// <summary>
-        /// Selects the same index in all Listboxes
-        /// </summary>
-        private void Lisbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            int i;
-            for (i = 0; i < listBoxes.Length; i++)
-            {
-                if (listBoxes[i] == sender)
-                    break;
-            }
-
-            for (int j = 0; j < listBoxes.Length; j++)
-            {
-                if (j != i)
-                {
-                    // Preventing looping
-                    listBoxes[j].SelectionChanged -= Lisbox_SelectionChanged;
-                    listBoxes[j].SelectedIndex = listBoxes[i].SelectedIndex;
-                    listBoxes[j].SelectionChanged += Lisbox_SelectionChanged;
-                }
-            }
+            mainDataGrid.DataContext = bookColection;
         }
 
         // TODO: Find out if this is needed
